3dab593ca3dd1adf224b28e1c88d74868fe7085a
vosahith <vosahith@lin65005403.corp.capgemini.com>
2025-01-03 11:50:02 +0530

    provider pact

--- demoApp1/src/test/java/com/example/demoApp1/pact/EmployeeProviderPact.java
+++ demoApp1/src/test/java/com/example/demoApp1/pact/EmployeeProviderPact.java
@@ -1,35 +1,43 @@
 package com.example.demoApp1.pact;
 
-import java.util.Map;
-
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import com.example.demoApp1.vo.EmployeeVO;
 
-@ExtendWith(PactProviderTestExt.class) // Extension for running provider tests with Pact
-@PactProvider("ProviderPact") 
+@Provider("ProviderPact") // Name of the provider
+@PactFolder("C:\\Users\\vosahith\\git\\Demo_app2\\demoApp2\\target\\pacts")
 public class EmployeeProviderPact {
 
-	 @RestController
-	    @RequestMapping("/api/employees")
-	    public static class EmployeeController {
-	        @GetMapping("/byId/1")
-	        public ResponseEntity<Map<String, Object>> getEmployee() {
-	            return ResponseEntity.ok(Map.of("id", 1, "name", "sahithi", "age", 30));
-	        }
-	    }
-
-	    @PactVerificationResult
-	    @Test
-	    void verifyPacts() {
-	        // Using PactVerifier to verify the contract against the provider
-	        PactVerifier pactVerifier = new PactVerifier();
-	        pactVerifier.setProvider("ProviderPact");
-
-	        // Check the contract against the provider
-	        pactVerifier.verify();
-	    }
+	@LocalServerPort
+    private int port;
+ 
+    public HttpTestTarget target ;
+ 
+    @BeforeEach
+    void setUp(PactVerificationContext context) {
+    	target = new HttpTestTarget("localhost", 8888 , "/"); // Dynamically set the port from the Spring Boot test
+        context.setTarget(target);
+    }
+     @State("Employee with ID 1 exists")
+      public void employeeWithId1Exists() {
+    	EmployeeVO empVO = new EmployeeVO();
+    	empVO.setId(1L);
+    	empVO.setName("sahithi");
+    	empVO.setAge(30);
+   }
+ 
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void verifyPacts(PactVerificationContext context) {
+        context.verifyInteraction(); // Verifies interactions defined in the Pact file
+    }
+    
 }
